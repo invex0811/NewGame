cf61b232a8f7f58986028493a336f498daf52efd
EscarvalOokt <escarval.ookt@gmail.com>
2024-04-05 21:23:43 +0300

    fix
    Удалил неиспользуемый метод контроллере персонажа

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -69,19 +69,6 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(PlayFootstepSound());
         }
     }
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Rigidbody body = hit.collider.attachedRigidbody;
-
-        if (body == null || body.isKinematic)
-            return;
-
-        if (hit.moveDirection.y < -0.3)
-            return;
-
-        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-        body.velocity = pushDir * 15;
-    }
     private IEnumerator PlayFootstepSound()
     {
         int clipIndex = Random.Range(0, _stepSounds.Length - 1);
